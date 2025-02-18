5660aa0740a79141e99b3e90e543fd6b1f0ee7e7
haseebismail703 <haseebismail703@gmail.com>
2025-02-18 12:24:25 +0500

    Create branch

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './Pages/PublicPages/Home';
 import UserRegister from './Pages/Auth/UserRegister';
 import UserLogin from './Pages/Auth/UserLogin';
+import ManageAdmin from './Pages/Admin/ManageAdmin';
 function App() {
   return (
     <div>
@@ -12,7 +13,8 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/Register" element={<UserRegister />} />
           <Route path="/login" element={<UserLogin />} />
-
+           {/* Admin route */}
+          <Route path="/admin/manage-admin" element={<ManageAdmin/>} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/Pages/Admin/ManageAdmin.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ManageAdmin() {
+  return (
+    <div>
+      bfdbfg
+    </div>
+  )
+}
